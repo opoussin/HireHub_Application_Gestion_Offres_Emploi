c4ff1c5ebda3dcb1b07eb00d3fd96329d661c733
chloe <chloe.gommard-dauba@etu.utc.fr>
2023-04-09 13:34:38 +0200

    WIP

--- myapp/Modele/Administrateur.js
+++ myapp/Modele/Administrateur.js
@@ -6,23 +6,47 @@ module.exports = {
             callback(results);
         });
     },
-    acceptOrga: function (callback) {
-        db.query("select * from Utilisateur", function (err, results) {
+    acceptOrga: function (nom, siren, type, siege_social, callback) {
+        sql = "SELECT Siren FROM USERS WHERE siren = ?";
+        rows = db.query(sql, email, function (err, results) {
             if (err) throw err;
-            callback(results);
+            if (rows.length == 1 ) {
+                callback(false)
+            } else {
+                var sql2 = mysql.format("INSERT INTO UTILISATEUR VALUES (?,?,?,?)", [nom, siren, type, siege_social]);
+                db.query(sql2, function (err, result) {
+                if (err) throw err;
+                callback(results);
+            });
+            }
         });
     },
-    updateStateCompte: function (callback) {
-        db.query("select * from Utilisateur", function (err, results) {
-            if (err) throw err;
-            callback(results);
-        });
+    desactiverCompte: function (callback) {
+        var sql = mysql.format("UPDATE UTILISATEUR SET Etat=False WHERE Mail=?");
+
+        db.query(sql, function (err, result) {
+                if (err) throw err;
+                callback(results);
+            });
+        callback(false);
     },
-    readAll: function (callback) {
-        db.query("select * from Utilisateur", function (err, results) {
-            if (err) throw err;
-            callback(results);
-        });
+    activerCompte: function (callback) {
+        var sql = mysql.format("UPDATE UTILISATEUR SET Etat=True WHERE Mail=?");
+
+        db.query(sql, function (err, result) {
+                if (err) throw err;
+                callback(results);
+            });
+        callback(false);
+    },
+    acceptAdmin: function (callback) {
+        var sql = mysql.format("UPDATE UTILISATEUR SET Type=3 WHERE Mail=?");
+
+        db.query(sql, function (err, result) {
+                if (err) throw err;
+                callback(results);
+            });
+        callback(false);
     },
 
 }
