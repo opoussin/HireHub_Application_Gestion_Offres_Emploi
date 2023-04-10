71322c90a6dd280e9ef577528c74ef7a6694386f
lespoussins60 <lespoussins60@penguin>
2023-04-10 16:02:31 +0200

    Update

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var userModel = require('../Modele/user.js')
+e ("/")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/Connexion', function (req, res, next) { 
+  result=userModel.read(function(result){
+    res.render('Connexion', { mail: result }); 
+  });
 });
 
 module.exports = router;
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var userModel = require('../Modele/user.js')
+var userModel = require('../Modele/Utilisateur.js')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -12,6 +12,12 @@ router.get('/userslist', function (req, res, next) { //route de readAll
   result=userModel.readall(function(result){
     res.render('usersList', { title: 'List des utilisateurs', users: result }); 
     /*vue userList, le deuxième paramètre de render est un objet qui contient toutes les variables qui seront utilisées dans la vue. */
+  });
+});
+
+router.get('/connexion', function (req, res, next) { 
+  result=userModel.read(function(result){
+    res.render('Connexion', {users: result }); 
   });
 });
 
