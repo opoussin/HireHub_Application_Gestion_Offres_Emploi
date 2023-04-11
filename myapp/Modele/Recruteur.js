ba6ebaa4e101f657a8e21966be5cf8da3b779e52
chloe <chloe.gommard-dauba@etu.utc.fr>
2023-04-11 23:05:45 +0200

    WIP

--- myapp/Modele/Recruteur.js
+++ myapp/Modele/Recruteur.js
@@ -19,4 +19,43 @@ readAllDmdRecruteur
 var db = require('./db.js');
 
 module.exports = {
+    updateOrga: function (mail, callback) {
+    
+    },
+    deleteOrga: function (mail, callback) {
+    
+    },
+    creatOffre: function (mail, callback) {
+    
+    },
+    deleteOffre: function (mail, callback) {
+    
+    },
+    readAllCandidat: function (mail, callback) {
+    
+    },
+    readAllOffreOrga: function (mail, callback) {
+    
+    },
+    updateOffreEtat: function (mail, callback) {
+    
+    },
+    creatFiche: function (mail, callback) {
+    
+    },
+    deleteFiche: function (mail, callback) {
+    
+    },
+    updateFiche: function (mail, callback) {
+    
+    },
+    acceptCandidat: function (mail, callback) {
+    
+    },
+    deleteRecruteurOrga: function (mail, callback) {
+    
+    },
+    readAllDmdRecruteur: function (mail, callback) {
+    
+    },
 }
