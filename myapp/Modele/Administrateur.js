286d83c337ad0a52d07096cef8058262db1935f4
chloe <chloe.gommard-dauba@etu.utc.fr>
2023-04-11 22:58:00 +0200

    WIP

--- myapp/Modele/Administrateur.js
+++ myapp/Modele/Administrateur.js
@@ -1,7 +1,7 @@
 /* 
 ADMIN
 readUser
-readUserAll
+readAllUser
 creatOrga
 acceptOrga
 disableUser
@@ -14,6 +14,73 @@ readAllDmdAdmin
 var db = require('./db.js');
 
 module.exports = {
+    readUser: function (mail, callback) {
+        db.query("select * from UTILISATEUR where mail= ?", mail, function
+            (err, results) {
+            if (err) throw err;
+            callback(results);
+        });
+    },
+    readAllUser: function (callback) {
+        db.query("select * from UTILISATEUR", function (err, results) {
+            if (err) throw err;
+            callback(results);
+        });
+    },
+    acceptOrga: function (nom, siren, type, siegesocial, callback) {
+        sql = "SELECT siren FROM UTILISATEURS WHERE siren = ?";
+        rows = db.query(sql, siren, function (err, results) {
+            if (err) throw err;
+            if (rows.length == 1 ) {
+                callback(false)
+            } else {
+                var sql2 = mysql.format("INSERT INTO UTILISATEUR VALUES (?,?,?,?)", [nom, siren, type, siegesocial]);
+                db.query(sql2, function (err, result) {
+                if (err) throw err;
+                callback(results);
+            });
+            }
+        });
+    },
+    disableUser: function (callback) {
+        var sql = mysql.format("UPDATE UTILISATEUR SET etat=0 WHERE mail=?");
+
+        db.query(sql, function (err, result) {
+                if (err) throw err;
+                callback(result);
+            });
+    },
+    enableUser: function (callback) {
+        var sql = mysql.format("UPDATE UTILISATEUR SET etat=1 WHERE mail=?");
+
+        db.query(sql, function (err, result) {
+                if (err) throw err;
+                callback(result);
+            });
+    },
+    acceptAdmin: function (callback) {
+        var sql = mysql.format("UPDATE UTILISATEUR SET type=3 WHERE mail=?");
+
+        db.query(sql, function (err, result) {
+                if (err) throw err;
+                callback(result);
+            });
+    },
+    creatOrga: function (mail, callback) {
+    
+    },
+    acceptOrga: function (mail, callback) {
+    
+    },
+    acceptRecruteur: function (mail, callback) {
+    
+    },
+    readAllDmdOrga: function (mail, callback) {
+    
+    },
+    readAllDmdAdmin: function (mail, callback) {
+    
+    },
 }
 
 /* ancienne version
--- myapp/Modele/Commun.js
+++ myapp/Modele/Commun.js
@@ -9,4 +9,52 @@ deleteUser
 var db = require('./db.js');
 
 module.exports = {
+    areUserValid: function (mail, mdp, callback) {
+        sql = "SELECT mdp FROM UTILISATEUR WHERE mail = ?";
+        db.query(sql, mail, function (err, rows) {
+            if (err) throw err;
+            if (rows.length == 1 && rows[0].mdp === mdp) {
+                callback(true)
+            } else {
+                callback(false);
+            }
+        });
+    },
+    creatUser: function (mail, nom, prenom, mdp, telephone, callback) {
+        //var date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+        var sql = mysql.format("INSERT INTO UTILISATEUR (mail, mdp, nom, prenom, telephone) VALUES (?,?,?,?,?)", [mail, mdp, nom, prenom, telephone]);
+
+        db.query(sql, function (err, results) {
+                if (err) throw err;
+                callback(results);
+            });
+    },
+    areRecruteur: function (mail, callback) {
+        sql = "SELECT Type FROM UTILISATEUR WHERE mail = ?";
+        rows = db.query(sql, mail, function (err, results) {
+            if (err) throw err;
+            if (rows.length == 1 && rows[0].type === 2) {
+                callback(true)
+            } else {
+                callback(false);
+            }
+        });
+    },
+    areAdmin: function (mail, callback) {
+        sql = "SELECT Type FROM USERS WHERE mail = ?";
+        rows = db.query(sql, mail, function (err, results) {
+            if (err) throw err;
+            if (rows.length == 1 && rows[0].type === 3) {
+                callback(true)
+            } else {
+                callback(false);
+            }
+        });
+    },
+    readOrga: function (mail, callback) {
+    
+    },
+    deleteUser: function (mail, callback) {
+    
+    },
 }
