05480a1ed5cdf70f5762a7793ff1e2f8531f29ef
chloe <chloe.gommard-dauba@etu.utc.fr>
2023-04-12 12:02:12 +0200

    WIP

--- myapp/Modele/Candidat.js
+++ myapp/Modele/Candidat.js
@@ -19,7 +19,7 @@ readUserDmdAdmin
 var db = require('./db.js');
 
 module.exports = {
-    update: function (mail, nom, prenom, telephone, callback) {
+    updateUser: function (mail, nom, prenom, telephone, callback) {
         /* if (result(email)||email==NULL){
             throw err;
         }else{ */
@@ -31,4 +31,100 @@ module.exports = {
             });
         //}
     },
+    readAllOffreValide: function (callback) {
+        db.query("select * from OFFRE INNER JOIN FICHE ON OFFRE.numero = FICHE.numero where OFFRE.etat= publiee", function
+            (err, results) {
+            if (err) throw err;
+            callback(results);
+        });
+    },
+    readOffreFiltre: function (mail, callback) {
+        
+    },
+    creatDmdOrga: function (nom, siren, type, siegeSocial, mail, callback) {
+        var sql = mysql.format("INSERT INTO DMD_ORGA (nom, siren, type, siegeSocial, recruteur) VALUES (?,?,?,?)", [nom, siren, type, siegeSocial, mail]);
+
+        db.query(sql, function (err, results) {
+            if (err) throw err;
+            callback(results);
+
+        });
+    },
+    creatDmdRecruteur: function (siren, mail, callback) {
+        var sql = mysql.format("INSERT INTO DMD_RECRUTEUR (organisation, recruteur) VALUES (?,?)", [siren, mail]);
+
+        db.query(sql, function (err, results) {
+            if (err) throw err;
+            callback(results);
+
+        });
+    },
+    creatDmdAdmin: function (mail, callback) {
+        var sql = mysql.format("INSERT INTO DMD_ADMIN (recruteur) VALUES (?)", [mail]);
+
+        db.query(sql, function (err, results) {
+            if (err) throw err;
+            callback(results);
+
+        });
+    },
+    creatCandidature: function (mail, callback) {
+        
+    },
+    deleteCandidature: function (mail, callback) {
+        
+    },
+    updateCandidature: function (mail, callback) {
+        
+    },
+    readAllCandidature: function (mail, callback) {
+        db.query("select * from CANDIDATURE where candidat=?", mail, function
+            (err, results) {
+            if (err) throw err;
+            callback(results);
+        });
+    },
+    deleteDmdOrga: function (siren, callback) {
+        db.query("DELETE * from DMD_ORGA where mail= ?", siren, function
+        (err, results) {
+        if (err) throw err;
+        callback(results);
+    });
+    },
+    deleteDmdRecruteur: function (mail, callback) {
+        db.query("DELETE * from DMD_RECRUTEUR where recruteur= ?", mail, function
+        (err, results) {
+        if (err) throw err;
+        callback(results);
+    });
+    },
+    deleteDmdAdmin: function (mail, callback) {
+        db.query("DELETE * from DMD_ADMIN where mail= ?", mail, function
+        (err, results) {
+        if (err) throw err;
+        callback(results);
+    });
+    },
+    readUserDmdOrga: function (mail, callback) {
+        db.query("select * from DMD_ORGA where recruteur=?", mail, function
+            (err, results) {
+            if (err) throw err;
+            callback(results);
+        });
+    },
+    readUserDmdRecruteur: function (mail, callback) {
+        db.query("select * from DMD_RECRUTEUR where recruteur=?", mail, function
+            (err, results) {
+            if (err) throw err;
+            callback(results);
+        });
+    },
+    readUserDmdAdmin: function (mail, callback) {
+        db.query("select * from DMD_ADMIN where candidat=?", mail, function
+            (err, results) {
+            if (err) throw err;
+            callback(results);
+        });
+    },
+
 }
