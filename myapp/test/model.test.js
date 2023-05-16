4c1c931381d88304e59599d3fc6dd774d1a9a73e
chloe <chloe.gommard-dauba@etu.utc.fr>
2023-05-16 18:52:28 +0200

    to do

--- myapp/test/model.test.js
+++ myapp/test/model.test.js
@@ -13,4 +13,28 @@ describe("Model Tests", () => {
     DB.end(callback); 
     }
     );
+    test ("read user",(done)=>{
+        model.read("test@test.fr", function (resultat){
+        nom = resultat[0].nom;
+        expect(nom).toBe("test");
+        })
+    function callback (err){
+    if (err) done (err);
+    else done();
+    }
+    DB.end(callback); 
+    }
+    );
+    test ("read user",(done)=>{
+        model.read("test@test.fr", function (resultat){
+        nom = resultat[0].nom;
+        expect(nom).toBe("test");
+        })
+    function callback (err){
+    if (err) done (err);
+    else done();
+    }
+    DB.end(callback); 
+    }
+    );
 })
