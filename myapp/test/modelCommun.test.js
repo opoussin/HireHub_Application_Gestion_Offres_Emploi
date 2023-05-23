67265594e9b6e0c548cbbf1d8df08fc33ffdf364
chloe <chloe.gommard-dauba@etu.utc.fr>
2023-05-23 22:32:38 +0200

    tests

--- myapp/test/modelCommun.test.js
+++ myapp/test/modelCommun.test.js
@@ -27,6 +27,8 @@ describe("Model Tests", () => {
 })
 */
 const model = require("../Modele/Commun.js");
+const DB = require("../Modele/db.js");
+
 // Import des dépendances pour les tests
 const assert = require('assert');
 const sinon = require('sinon');
@@ -37,52 +39,105 @@ const nom = 'John';
 const prenom = 'Doe';
 const telephone = '1234567890';
 const callback = sinon.stub();
-
-
+const mdp = 123;
+const orga = "exemple";
+/*
 describe("Model Tests", () => {
-  // ...
+    test ("read user",(done)=>{
+        model.readUser("chloe@go", function (resultat){
+        nom = resultat[0].nom;
+        expect(nom).toBe("Gommard");
+        })
+    function callback (err){
+    if (err) done (err);
+    else done();
+    }
+    DB.end(callback); 
+    }
+    );
+    test ("read user",(done)=>{
+      model.readUser("chloe@go", function (resultat){
+      nom = resultat[0].nom;
+      expect(nom).toBe("Gommard");
+      })
+    function callback (err){
+    if (err) done (err);
+    else done();
+    }
+    DB.end(callback); 
+    }
+    );
+})
+*/
+describe("Model Tests", () => {
+  afterAll((done) => {
+    function callback(err) {
+      if (err) done(err);
+      else done();
+    }
+    DB.end(callback);
+  });
+    test("creatUser", (done) => {
+        model.creatUser(mail, nom, prenom, mdp, telephone, function (err) {
+        expect(err).toBeFalsy(); // Vérifie si aucune erreur n'est survenue
+        done();
+        });
+    });
+    /* pas utilisée je crois
+  test("areUserValid", (done) => {
+    model.areUserValid("example@mail.com", "password", function (result) {
+      expect(result).toBeDefined(); // Vérifie si le résultat est défini (utilisateur valide)
+      done();
+    });
+  });*/
 
-  test("updateUser", (done) => {
-    model.updateUser("example@mail.com", "John", "Doe", "123456789", function (err, result) {
-      expect(err).toBeFalsy(); // Vérifie si aucune erreur n'est survenue
-      expect(result).toBeDefined(); // Vérifie si le résultat est défini
+
+  test("areRecruteur", (done) => {
+    model.areRecruteur(mail, function (result) {
+      expect(result).toBe(false); // Vérifie si le résultat est faux (non recruteur)
       done();
     });
   });
 
-  test("updateUserMdp", (done) => {
-    model.updateUserMdp("newpassword", "example@mail.com", function (err) {
-      expect(err).toBeFalsy(); // Vérifie si aucune erreur n'est survenue
+  test("areAdmin", (done) => {
+    model.areAdmin(mail, function (result) {
+      expect(result).toBe(false); // Vérifie si le résultat est faux (non administrateur)
       done();
     });
   });
 
-  test("readAllOffreValide", (done) => {
-    model.readAllOffreValide(function (results) {
-      expect(results).toBeDefined(); // Vérifie si le résultat est défini
-      expect(Array.isArray(results)).toBe(true); // Vérifie si le résultat est un tableau
+  test("readOrga", (done) => {
+    model.readOrga("exampleOrga", function (results) {
+      expect(results).toHaveLength(0); // Vérifie si aucun résultat n'est retourné (0 élément)
       done();
     });
   });
 
-  test("readOffreFiltre", (done) => {
-    model.readOffreFiltre("exampleOrga", "Paris", "CDI", "50000", "Temps plein", "Titre", function (results) {
-      expect(results).toBeDefined(); // Vérifie si le résultat est défini
-      expect(Array.isArray(results)).toBe(true); // Vérifie si le résultat est un tableau
+
+
+  test("readUser", (done) => {
+    model.readUser("chloe@go", function (result) {
+      expect(result).toBeDefined(); // Vérifie si le résultat est défini (utilisateur trouvé)
       done();
     });
   });
 
-  test("creatDmdOrga", (done) => {
-    model.creatDmdOrga("Nom Orga", "123456789", "Type Orga", "Siège Social", "example@mail.com", function (results) {
-      expect(results).toBeDefined(); // Vérifie si le résultat est défini
+  test("readOrgaUser", (done) => {
+    model.readOrgaUser("chloe@go", function (results) {
+      expect(results).toHaveLength(1); // Vérifie si aucun résultat n'est retourné (0 élément)
       done();
     });
+    model.readOrgaUser("ddd@ooo", function (results) {
+        expect(results).toHaveLength(0); // Vérifie si aucun résultat n'est retourné (0 élément)
+        done();
+      });
   });
   
-
-  // ...
-
-  // Ajoutez les autres tests pour les autres fonctions du module
-  
+  test("deleteUser", (done) => {
+    model.deleteUser(mail, function () {
+      // Vérifie si la suppression de l'utilisateur s'est déroulée avec succès
+      expect(err).toBeFalsy();
+      done();
+    });
+  });
 });
--- myapp/test/modeleCandidat.test.js
+++ myapp/test/modeleCandidat.test.js
@@ -39,75 +39,194 @@ describe("Model Tests", () => {
 */
 
 const DB = require("../Modele/db.js");
-const model = require("../Modele/Commun.js");
+const model = require("../Modele/Candidat.js");
 
-describe("Model Tests", () => {
-  afterAll((done) => {
-    function callback(err) {
-      if (err) done(err);
-      else done();
-    }
-    DB.end(callback);
-  });
-    test("creatUser", (done) => {
-        model.creatUser("example@mail.com", "John", "Doe", "password", "123456789", function (err) {
-        expect(err).toBeFalsy(); // Vérifie si aucune erreur n'est survenue
-        done();
-        });
-    });
-
-  test("areUserValid", (done) => {
-    model.areUserValid("example@mail.com", "password", function (result) {
-      expect(result).toBeDefined(); // Vérifie si le résultat est défini (utilisateur valide)
-      done();
-    });
+describe('Model Tests', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
+  test('updateUser updates user information', () => {
+    const mail = 'test@example.com';
+    const nom = 'John';
+    const prenom = 'Doe';
+    const telephone = '1234567890';
+    const callback = jest.fn();
 
-  test("areRecruteur", (done) => {
-    model.areRecruteur("example@mail.com", function (result) {
-      expect(result).toBe(false); // Vérifie si le résultat est faux (non recruteur)
-      done();
+    const mockDbQuery = jest.spyOn(DB, 'query').mockImplementation((sql, params, callback) => {
+      // Verify if the SQL query is correct
+      expect(sql).toBe('UPDATE UTILISATEUR SET nom =?, prenom=?, telephone=? WHERE mail=?');
+      // Verify if the parameters are correct
+      expect(params).toEqual([nom, prenom, telephone, mail]);
+
+      // Call the callback without an error
+      callback(/* error */);
     });
+
+    model.updateUser(mail, nom, prenom, telephone, callback);
+
+    // Verify if the function DB.query was called with the correct parameters
+    expect(mockDbQuery).toHaveBeenCalledWith(expect.any(String), expect.any(Array), expect.any(Function));
+
+    // Verify if the callback was called without an error
+    expect(callback).toHaveBeenCalledWith(/* error */);
+
+    // Restore the original DB.query function
+    mockDbQuery.mockRestore();
   });
 
-  test("areAdmin", (done) => {
-    model.areAdmin("example@mail.com", function (result) {
-      expect(result).toBe(false); // Vérifie si le résultat est faux (non administrateur)
-      done();
+  test('updateUserMdp updates user password', () => {
+    const mdp1 = 'newpassword';
+    const mail = 'test@example.com';
+    const callback = jest.fn();
+
+    const mockDbQuery = jest.spyOn(DB, 'query').mockImplementation((sql, params, callback) => {
+      // Verify if the SQL query is correct
+      expect(sql).toBe('UPDATE UTILISATEUR SET mdp =? WHERE mail=?');
+      // Verify if the parameters are correct
+      expect(params).toEqual([mdp1, mail]);
+
+      // Call the callback without an error
+      callback(/* error */);
     });
+
+    model.updateUserMdp(mdp1, mail, callback);
+
+    // Verify if the function DB.query was called with the correct parameters
+    expect(mockDbQuery).toHaveBeenCalledWith(expect.any(String), expect.any(Array), expect.any(Function));
+
+    // Verify if the callback was called without an error
+    expect(callback).toHaveBeenCalledWith(/* error */);
+
+    // Restore the original DB.query function
+    mockDbQuery.mockRestore();
   });
 
-  test("readOrga", (done) => {
-    model.readOrga("exampleOrga", function (results) {
-      expect(results).toHaveLength(0); // Vérifie si aucun résultat n'est retourné (0 élément)
-      done();
+  test('readAllOffreValide retrieves all valid offers', () => {
+    const callback = jest.fn();
+
+    const mockDbQuery = jest.spyOn(DB, 'query').mockImplementation((sql, callback) => {
+      // Verify if the SQL query is correct
+      expect(sql).toBe("SELECT * FROM OFFRE INNER JOIN FICHE_POSTE ON OFFRE.numero = FICHE_POSTE.offre INNER JOIN ORGANISATION ON ORGANISATION.siren=OFFRE.organisation WHERE OFFRE.etat='publiee'");
+
+      // Call the callback with a fake result
+      callback(null, /* fake result */);
     });
+
+    model.readAllOffreValide(callback);
+
+    // Verify if the function DB.query was called with the correct parameters
+    expect(mockDbQuery).toHaveBeenCalledWith(expect.any(String), expect.any(Function));
+
+    // Verify if the callback was called with the fake result
+    expect(callback).toHaveBeenCalledWith(/* fake result */);
+
+    // Restore the original DB.query function
+    mockDbQuery.mockRestore();
   });
 
+  test('readOffreFiltre retrieves filtered offers', () => {
+    const organisation = 'Example Org';
+    const lieu = 'Paris';
+    const statut = 'Full-time';
+    const salaire = '50000';
+    const type = 'Software Engineer';
+    const intitule = 'Job Title';
+    const callback = jest.fn();
 
+    const mockDbQuery = jest.spyOn(DB, 'query').mockImplementation((sql, callback) => {
+      // Verify if the SQL query is correct
+      expect(sql).toContain("SELECT * FROM OFFRE INNER JOIN FICHE_POSTE ON OFFRE.numero = FICHE_POSTE.offre INNER JOIN ORGANISATION ON ORGANISATION.siren=OFFRE.organisation WHERE OFFRE.etat='publiee'");
+      expect(sql).toContain(`AND FICHE_POSTE.intitule = '${intitule}'`);
+      expect(sql).toContain(`AND ORGANISATION.nom = '${organisation}'`);
+      expect(sql).toContain(`AND FICHE_POSTE.lieu = '${lieu}'`);
+      expect(sql).toContain(`AND FICHE_POSTE.statut = '${statut}'`);
+      expect(sql).toContain(`AND FICHE_POSTE.type > ${salaire}`);
+      expect(sql).toContain(`AND FICHE_POSTE.type = '${type}'`);
 
-  test("readUser", (done) => {
-    model.readUser("chloe@go", function (result) {
-      expect(result).toBeDefined(); // Vérifie si le résultat est défini (utilisateur trouvé)
-      done();
+      // Call the callback with a fake result
+      callback(null, /* fake result */);
     });
+
+    model.readOffreFiltre(organisation, lieu, statut, salaire, type, intitule, callback);
+
+    // Verify if the function DB.query was called with the correct parameters
+    expect(mockDbQuery).toHaveBeenCalledWith(expect.any(String), expect.any(Function));
+
+    // Verify if the callback was called with the fake result
+    expect(callback).toHaveBeenCalledWith(/* fake result */);
+
+    // Restore the original DB.query function
+    mockDbQuery.mockRestore();
   });
 
-  test("readOrgaUser", (done) => {
-    model.readOrgaUser("chloe@go", function (results) {
-      expect(results).toHaveLength(1); // Vérifie si aucun résultat n'est retourné (0 élément)
-      done();
+  test('creatDmdOrga creates a new organization request', () => {
+    const nom = 'New Org';
+    const siren = '123456789';
+    const type = 'Public';
+    const siegeSocial = 'Paris';
+    const mail = 'test@example.com';
+    const callback = jest.fn();
+
+    const mockDbQuery = jest.spyOn(DB, 'query').mockImplementation((sql, params, callback) => {
+      if (sql.startsWith('SELECT')) {
+        // Simulate an existing organization
+        callback(null, /* fake result */);
+      } else {
+        // Verify if the SQL query is correct
+        expect(sql).toBe("INSERT INTO DMD_ORGA (nom, siren, type, siegeSocial, recruteur) VALUES (?,?,?,?,?)");
+        // Verify if the parameters are correct
+        expect(params).toEqual([nom, siren, type, siegeSocial, mail]);
+
+        // Call the callback with a fake result
+        callback(null, /* fake result */);
+      }
     });
-    model.readOrgaUser("ddd@ooo", function (results) {
-        expect(results).toHaveLength(0); // Vérifie si aucun résultat n'est retourné (0 élément)
-        done();
-      });
+
+    model.creatDmdOrga(nom, siren, type, siegeSocial, mail, callback);
+
+    // Verify if the function DB.query was called with the correct parameters
+    expect(mockDbQuery).toHaveBeenCalledWith(expect.any(String), expect.any(Array), expect.any(Function));
+
+    // Verify if the callback was called with the fake result
+    expect(callback).toHaveBeenCalledWith(/* fake result */);
+
+    // Restore the original DB.query function
+    mockDbQuery.mockRestore();
   });
   
-  test("deleteUser", (done) => {
-    model.deleteUser("example@mail.com", function () {
-      // Vérifie si la suppression de l'utilisateur s'est déroulée avec succès
+  test("updateUser", (done) => {
+    model.updateUser("chloe@go", "Gommard", "Chloe", "123456789", (result) => {
+      expect(result).toBe(/* expected result */);
+      done();
+    });
+  });
+
+  test("updateUserMdp", (done) => {
+    model.updateUserMdp("newPassword", "chloe@go", () => {
+      // Check if the update was successful
+      // ...
+      done();
+    });
+  });
+
+  test("readAllOffreValide", (done) => {
+    model.readAllOffreValide((results) => {
+      expect(results).toEqual(/* expected results */);
+      done();
+    });
+  });
+
+  test("readOffreFiltre", (done) => {
+    model.readOffreFiltre("Orga", "Lieu", "Statut", "Salaire", "Type", "Intitule", (results) => {
+      expect(results).toEqual(/* expected results */);
+      done();
+    });
+  });
+
+  test("creatDmdOrga", (done) => {
+    model.creatDmdOrga("Nom", "123456789", "Type", "SiegeSocial", "mail@example.com", (results) => {
+      expect(results).toBe(/* expected result */);
       done();
     });
   });
